15216ef03afdcdb3b4ab08e1146eff7c095dc814
ruki <waruqi@gmail.com>
2017-05-24 22:36:53 +0800

    fix compile error on msvc

--- include/semver.h
+++ include/semver.h
@@ -31,7 +31,6 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 
 #ifndef SV_COMPILE
 # define SV_COMPILE (0)
@@ -71,6 +70,26 @@
 # else
 #   include <stdbool.h>
 # endif
+#endif
+
+#if defined(_MSC_VER)
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#ifdef _WIN64
+typedef __int64 intptr_t;
+typedef unsigned __int64 uintptr_t;
+#else
+typedef __int32 intptr_t;
+typedef unsigned __int32 uintptr_t;
+#endif
+#else
+#   include <stdint.h>
 #endif
 
 #ifndef sv_malloc
