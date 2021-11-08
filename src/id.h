705c2b42c89f57978b3c9e6b1cbb94f1e629cd6f
ruki <waruqi@gmail.com>
2021-11-08 22:57:52 +0800

    fix parse build

--- src/id.h
+++ src/id.h
@@ -32,7 +32,8 @@
 
 void semver_id_ctor(semver_id_t *self);
 void semver_id_dtor(semver_id_t *self);
-char semver_id_read(semver_id_t *self, const char *str, size_t len, size_t *offset);
+char semver_id_read_prerelease(semver_id_t *self, const char *str, size_t len, size_t *offset);
+char semver_id_read_build(semver_id_t *self, const char *str, size_t len, size_t *offset);
 int  semver_id_pwrite(const semver_id_t *self, char *buffer, size_t len);
 int semver_id_pcmp(const semver_id_t *self, const semver_id_t *other);
 
