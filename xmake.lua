3f7097a9575ae732a43de1481eb45f0ec31a15e8
ruki <waruqi@gmail.com>
2025-09-04 22:37:47 +0800

    Update xmake.lua (#60)

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,14 @@
 set_project("libsv")
 set_version("0.0.1")
+set_xmakever("3.0.1")
 
+add_rules("mode.debug", "mode.release")
 set_warnings("all", "error")
 
-if is_plat("macosx") or is_plat("iphoneos") then
+if is_plat("macosx", "iphoneos") then
     add_cflags("-Wno-nullability-completeness")
     add_cxxflags("-Wno-nullability-completeness")
 end
-
-add_rules("mode.debug", "mode.release")
 
 target("sv")
     set_default(true)
